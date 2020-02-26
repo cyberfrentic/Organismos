7430e9e064e0523f71167f9e7e5473e543cab7cf
cyberfrentic <j_hugo_p_c3@msn.com>
2020-02-26 11:02:31 -0600

    ultima actualizacion

--- src/almacen/urls.py
+++ src/almacen/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('fcp/', include('fcp.urls')),
     path('autho/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='./registration/login.html'), name='home'),
 ]
 
 if settings.DEBUG:
